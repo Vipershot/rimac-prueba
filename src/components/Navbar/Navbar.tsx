d97af0184f5583db7962f1bf60d32e9ed1abad21
Vipershot <imvipershot@gmail.com>
2023-11-15 16:30:53 -0500

    feat: :rocket: create proyect and config sass
    create proyect with vite and scaffolding change style for scss
    
    Jean

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  return (
+    <div className=" navbar_container">
+      Navbar
+      <p>texto prueba</p>
+    </div>
+  );
+};
+
+export default Navbar;
