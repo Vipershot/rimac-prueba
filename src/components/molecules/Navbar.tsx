f83cccf5564022d341f3edddf10396cb5718c3bf
Vipershot <imvipershot@gmail.com>
2023-11-16 16:42:39 -0500

    feat: :art: create navbar for web
    create navbar for web

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
 import { TextMain } from '../atoms/TextMain'
 
 export const Navbar = () => {
   return (
     <div className='navbar-container'>
       <div className='navbar-container nav-brand'>
-        <img src='../src/assets/LogoRIMAC.svg' alt='' />
+        <img src='../src/assets/LogoRIMAC.svg' alt='Rimac' />
       </div>
-      <div className='navbar-container nav-contact'>
+      {/* Mobile */}
+      <div className='navbar-container nav-contact-mobile'>
         <img src='../src/assets/Telf.svg' alt='' />
         <TextMain size='sm' category='p' color='secondary' weight='light' text='Llámanos' />
+      </div>
+      {/* Web */}
+      <div className='navbar-container nav-contact-web'>
+        <TextMain
+          size='sm'
+          category='p'
+          color='primary'
+          weight='light'
+          text='¿Tienes alguna duda?'
+        />
+        <div className='nav-phone'>
+          <img src='../src/assets/Telf.svg' alt='' />
+          <TextMain size='sm' category='p' color='secondary' weight='light' text='(01) 411 6001' />
+        </div>
       </div>
     </div>
   )
