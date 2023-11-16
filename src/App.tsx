087b88eb6af08ce31388aa7fab6ee2d96c2ddef0
Vipershot <imvipershot@gmail.com>
2023-11-15 20:24:08 -0500

    feat: :construction: config axios
    config axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Navbar from './components/Navbar/Navbar'
+import axios from "./axios";
 
 
 function App() {
+
+axios.get('/users').then(response => {
+ console.log(response.data);
+}).catch(error => {
+ console.error(error);
+});
 
 
   return (
